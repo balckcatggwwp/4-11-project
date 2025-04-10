9f3ee8416b95de2f7d80b099214f7123cca930a4
LAPTOP-D9NJU2CB\user <ericchung8609@gmail.com>
2025-04-10 16:52:24 +0800

    修改錯誤

--- src/main/java/com/example/demo/member/model/Member.java
+++ src/main/java/com/example/demo/member/model/Member.java
@@ -33,7 +33,7 @@ public class Member {
 	@Column(name = "gender",nullable = false)
 	private String gender;
 	
-	@Column(name = "nationalId",nullable = false,length = 10,unique = true)
+	@Column(name = "nationalId",nullable = false,length = 10)
 	private String nationalId;
 	
 //	認證相關
