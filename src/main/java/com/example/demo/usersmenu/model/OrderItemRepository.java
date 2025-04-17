17e4dc6a1b3c9b0aceb7b071434e32853e309f7e
Timmy <timmychen520@gmail.com>
2025-04-17 15:08:34 +0800

    123

--- src/main/java/com/example/demo/usersmenu/model/OrderItemRepository.java
+++ src/main/java/com/example/demo/usersmenu/model/OrderItemRepository.java
@@ -8,10 +8,21 @@ import org.springframework.data.repository.query.Param;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
 
-
-
 	@Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.menu WHERE oi.order = :order")
 	List<OrderItem> findItemsWithMenuByOrder(@Param("order") OrderHeader order);
 
-	 List<OrderItem> findByOrderOrderId(Integer orderId);
+	List<OrderItem> findByOrderOrderId(Integer orderId);
+
+	@Query(value = "SELECT TOP 5 m.menu_name, SUM(oi.quantity) AS total " +
+			"FROM order_item oi JOIN usersmenu m ON oi.menu_id = m.menu_id " +
+			"GROUP BY m.menu_name ORDER BY total DESC", nativeQuery = true)
+	List<Object[]> findTop5MenuSales();
+
+	@Query(value = "SELECT TOP 5 m.menu_name, SUM(oi.quantity) AS total " +
+			"FROM order_item oi JOIN usersmenu m ON oi.menu_id = m.menu_id " +
+			"GROUP BY m.menu_name ORDER BY total ASC", nativeQuery = true)
+	List<Object[]> findBottom5MenuSales();
+	
+
+
 }
