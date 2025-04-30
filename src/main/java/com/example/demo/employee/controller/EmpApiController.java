e576890eb47afa4cd0591979d77580a34a55460a
LAPTOP-D9NJU2CB\user <ericchung8609@gmail.com>
2025-04-30 11:06:36 +0800

    新增初始管理員新增 需先在後台 員工權限/員工職位 權限(有的權限都新增)/職位 最高管理員/等級五

--- src/main/java/com/example/demo/employee/controller/EmpApiController.java
+++ src/main/java/com/example/demo/employee/controller/EmpApiController.java
@@ -1,10 +1,51 @@
 package com.example.demo.employee.controller;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.employee.model.Employee;
+import com.example.demo.employee.model.EmployeeRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/employeeApi")
 public class EmpApiController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/sessionInfo")
+	public Map<String, Object> getSessionEmployee(HttpSession session) {
+		UUID empId = (UUID) session.getAttribute("empId");
+		if (empId == null)
+			return Map.of("status", "error");
 
+		Employee emp = employeeRepository.findById(empId).orElse(null);
+		if (emp == null)
+			return Map.of("status", "error");
+
+		return Map.of("status", "success", "name", emp.getName(), "jobTitleName",
+				emp.getJobTitleCategory().getJobTitleName(), "entryTime", emp.getEntryTime(), "gender", emp.getGender(),
+				"phoneNumber", emp.getPhoneNumber());
+	}
+	@GetMapping("/sessionInfoForTopbar")
+	public Map<String, Object> sessionInfoForTopbar(HttpSession session) {
+		UUID empId = (UUID) session.getAttribute("empId");
+		if (empId == null)
+			return Map.of("status", "error");
+
+		Employee emp = employeeRepository.findById(empId).orElse(null);
+		if (emp == null)
+			return Map.of("status", "error");
+
+		return Map.of("status", "success", "name", emp.getName());
+	}
 }
--- src/main/java/com/example/demo/employee/controller/EmpController.java
+++ src/main/java/com/example/demo/employee/controller/EmpController.java
@@ -6,16 +6,19 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import com.example.demo.employee.model.AdminRegisterDTO;
 import com.example.demo.employee.model.Employee;
 import com.example.demo.employee.model.EmployeeRepository;
 import com.example.demo.employee.model.EmployeeService;
@@ -27,97 +30,79 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/employee")
 public class EmpController {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
 	@Autowired
-	
+
 	private JobTitleCategoryRepository jobTitleCategoryRepository;
-	
+
+
 	@GetMapping("/empLogin")
 	public String empLogin() {
 		return "employee/empLogin";
 	}
-	
-	
-	
+
 	@GetMapping("/empIndex")
 	public String empIndex() {
 		return "employee/empIndex";
 	}
-	
-	@GetMapping("/empRegister")
+
+	@GetMapping("/adminRegister")
 	public String empRegister() {
-		return "employee/empRegister";
+		return "employee/adminRegister";
 	}
-	
+
 	@PostMapping("/empRegister")
-	public ResponseEntity<?> register(@ModelAttribute Employee employee){
+	public ResponseEntity<?> register(@ModelAttribute Employee employee) {
 		boolean result = employeeService.empExistCheck(employee);
 		System.out.println(result);
 		if (result) {
-	        Employee insertEmployee = employeeService.insertEmployee(employee);
-	        if (insertEmployee != null) {
-	        	System.out.println("可以註冊");
-	            return ResponseEntity.ok(Map.of(
-	                "status", "success",
-	                "message", "註冊成功",
-	                "name", insertEmployee.getName(),
-	                "email", insertEmployee.getEmail()
-	            ));
-	        } else {
-	            return ResponseEntity
-	                .badRequest()
-	                .body(Map.of("status", "error", "message", "註冊失敗，新增失敗"));
-	        }
-	    } else {
-	        return ResponseEntity
-	            .badRequest()
-	            .body(Map.of("status", "error", "message", "電子信箱或手機號碼已被使用"));
-	    }
+			Employee insertEmployee = employeeService.insertEmployee(employee);
+			if (insertEmployee != null) {
+				System.out.println("可以註冊");
+				return ResponseEntity.ok(Map.of("status", "success", "message", "註冊成功", "name",
+						insertEmployee.getName(), "email", insertEmployee.getEmail()));
+			} else {
+				return ResponseEntity.badRequest().body(Map.of("status", "error", "message", "註冊失敗，新增失敗"));
+			}
+		} else {
+			return ResponseEntity.badRequest().body(Map.of("status", "error", "message", "電子信箱或手機號碼已被使用"));
+		}
 	}
-	
-	
+
 	@GetMapping("/insertSubordinate")
 	public String registerPage(Model model, @SessionAttribute("empDetail") Employee loginEmp) {
-	    int loginLevel = loginEmp.getJobTitleCategory().getJobLevel();
-	    List<JobTitleCategory> lowerTitles = jobTitleCategoryRepository.findByJobLevelLessThan(loginLevel);
-	    model.addAttribute("jobTitleOptions", lowerTitles);
-	    return "employee/registerForm";
+		int loginLevel = loginEmp.getJobTitleCategory().getJobLevel();
+		List<JobTitleCategory> lowerTitles = jobTitleCategoryRepository.findByJobLevelLessThan(loginLevel);
+		model.addAttribute("jobTitleOptions", lowerTitles);
+		return "employee/registerForm";
 	}
-	
+
 	@PostMapping("/empLogin")
 	@ResponseBody
-	public Map<String, Object> empLogin(@RequestParam("email") String email,
-	                                 @RequestParam("password") String password,
-	                                 HttpSession httpSession) {
-	    Map<String, Object> response = new HashMap<>();
-	    Employee LoginBean = employeeService.login(email, password);
-	    if (LoginBean != null) {
-	    	
-	    	
-	    	httpSession.setAttribute("empDetail", LoginBean);
-	    	httpSession.setAttribute("empName", LoginBean.getName());
-	    	httpSession.setAttribute("empEmail", LoginBean.getEmail());
-	        response.put("status", "success");
-	        response.put("empName", LoginBean.getName());
-	    } else {
-	        response.put("status", "error");
-	        response.put("message", "帳號或密碼錯誤");
-	    }
-	    return response;
+	public Map<String, Object> empLogin(@RequestParam("email") String email, @RequestParam("password") String password,
+			HttpSession httpSession) {
+		Map<String, Object> response = new HashMap<>();
+		Employee LoginBean = employeeService.login(email, password);
+		if (LoginBean != null) {
+			httpSession.setAttribute("empId", LoginBean.getEmpId());
+			response.put("status", "success");
+			response.put("empName", LoginBean.getName());
+		} else {
+			response.put("status", "error");
+			response.put("message", "帳號或密碼錯誤");
+		}
+		return response;
 	}
-	
+
 	@GetMapping("/empLogout")
 	public String empLogout(HttpSession httpSession) {
-		httpSession.removeAttribute("empDetail");
-    	httpSession.removeAttribute("empName");
-    	httpSession.removeAttribute("empEmail");
+		httpSession.removeAttribute("empId");
 		return "employee/empLogin";
 	}
 	
@@ -125,18 +110,45 @@ public class EmpController {
 	public String empList() {
 		return "employee/empList";
 	}
-	
+
 	@GetMapping("/empManager")
 	public String empManager() {
 		return "employee/empManager";
 	}
-	//員工權限管理
+
+	// 員工權限管理
 	@GetMapping("/permissions")
 	public String permissionPage(Model model, @SessionAttribute("empDetail") Employee loginEmp) {
-	    int loginLevel = loginEmp.getJobTitleCategory().getJobLevel();
-	    List<Employee> subordinates = employeeRepository.findByJobTitleCategoryJobLevelLessThan(loginLevel);
-	    model.addAttribute("subordinates", subordinates);
-	    return "employee/permissionPage";
+		int loginLevel = loginEmp.getJobTitleCategory().getJobLevel();
+		List<Employee> subordinates = employeeRepository.findByJobTitleCategoryJobLevelLessThan(loginLevel);
+		model.addAttribute("subordinates", subordinates);
+		return "employee/permissionPage";
 	}
 
+	// 員工管理職務網頁
+	@GetMapping("/jobTitleManager")
+	public String jobTitleManager() {
+		return "employee/jobTitleManager";
+	}
+
+	// 員工權限新增empPermissionList.html
+	@GetMapping("/empPermissionList")
+	public String empPermissionList() {
+		return "employee/empPermissionList";
+	}
+
+	// 處理 AJAX 註冊請求（POST JSON）
+	@PostMapping("/adminRegister")
+	@ResponseBody
+	public Map<String, Object> registerAdmin(@RequestBody AdminRegisterDTO dto) {
+		Map<String, Object> res = new HashMap<>();
+		try {
+			employeeService.registerAdmin(dto); // 實際邏輯請實作在 service 裡
+			res.put("status", "success");
+		} catch (Exception e) {
+			res.put("status", "error");
+			res.put("message", e.getMessage());
+		}
+		return res;
+	}
 }
